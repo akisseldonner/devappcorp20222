c1cd0933f8ef6095a0eb8168747615204a89b458
Akissel <akisselsilva@gmail.com>
2022-12-21 02:47:43 -0300

    Criando a camada de serviço para usuário
    Apenas listando alguns metodos basicos a serem obtidos atraves JPARepository

---
+++ src/main/java/akisselplayer/com/demo/domain/entity/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package akisselplayer.com.demo.domain.entity.service;
+
+import akisselplayer.com.demo.domain.entity.Usuario;
+import akisselplayer.com.demo.domain.entity.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.List;
+@Service
+@Transactional //rollback para erros no meio de um pacote de transações
+@RequiredArgsConstructor
+public class UsuarioService {
+    // @Autowired não necessaria ja que usamos o lombok la em cima
+    private UsuarioRepository repository;
+
+    public Usuario salvar(Usuario usuario){
+        // implementar validação de login e email exclusivos
+        return repository.save(usuario);
+    }
+
+    public List<Usuario> listarTodos(){
+        return repository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorID(Long id){
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
+}
